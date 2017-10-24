bbefe603f6899dfb0fff277f7050e125159aee86
Steven D. Shelton <steven-shelton@utc.edu>
2017-10-24 17:13:43 -0400

    first commit

---
+++ 01UTC_INST-01UTC/js/custom.js
@@ -0,0 +1,38 @@
+(function() {
+  "use strict";
+  'use strict';
+
+
+  var app = angular.module('viewCustom', ['angularLoad']);
+
+  /****************************************************************************************************/
+
+  /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+
+  /*var app = angular.module('centralCustom', ['angularLoad']);*/
+
+  /****************************************************************************************************/
+
+  app.controller('prmLogoAfterController', [function() {
+    var vm = this;
+    vm.getIconLink = getIconLink;
+
+    function getIconLink() {
+      return vm.parentCtrl.iconLink;
+    }
+  }]);
+
+
+  //update template to include new URL for institution
+  app.component('prmLogoAfter', {
+    bindings: {
+      parentCtrl: '<'
+    },
+    controller: 'prmLogoAfterController',
+    template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner"><a href="https://www.utc.edu/library/"><img class="logo-image" alt="{{::(\'nui.header.LogoAlt\' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/></a></div>'
+  });
+
+
+
+
+})();

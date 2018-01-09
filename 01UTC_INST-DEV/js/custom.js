77e112d08cd0994f9419dfbdbbc73402b95f9a76
Steven D. Shelton <steven-shelton@utc.edu>
2018-01-09 15:42:56 -0500

    modify CSS to highlight View Online in DEV

--- 01UTC_INST-DEV/js/custom.js
+++ 01UTC_INST-DEV/js/custom.js
@@ -1,4 +1,3 @@
-
 (function() {
   "use strict";
   'use strict';
@@ -7,13 +6,13 @@
 
 
   // ADD chat to no results
+  /*
   app.component('prmNoSearchResultAfter',{
     bindings: {parentCtrl: '<'},
     controller: 'prmNoSearchResultAfterController',
     template: '<md-card class="default-card"><md-card-title><md-card-title-text><span class="md-headline">Get Help</span></md-card-title-text></md-card-title><md-card-content><div class="needs-js iframe normal">JavaScript is loading.</div></md-card-content></md-card>'
   });
-
-
+*/
   /****************************************************************************************************/
 
   /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
@@ -44,43 +43,43 @@
   app.component('prmSearchBarAfter', {
    template: '<br/><hr/><p style="text-align: center; font-size: 20px; color: white;">Please note that access to online resources will be unavailable this evening from 8pm-9pm.</p><hr/>'
  });*/
-  /* add ILL to card menu */
-  app.component('prmLibraryCardMenuAfter', {
-    template:  '<button class="button-with-icon zero-margin md-button md-primoExplore-theme md-ink-ripple" type="button" aria-label="Go To My ILL account" href="https://utc.illiad.oclc.org/illiad/logon.html"><span>ILL Account</span></button>'
-  });
 })();
-
-//chat box
-setInterval(function() {
-(function() {
-  var x = document.createElement("script"); x.type = "text/javascript"; x.async = true;
-  x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.libraryh3lp.com/js/libraryh3lp.js?4949";
-  var y = document.getElementsByTagName("script")[0]; y.parentNode.insertBefore(x, y);
-})();
-}, 50);
 /* Load JQuery */
-
-var js = document.createElement('script') ;
+var js = document.createElement('script');
 js.src = "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js";
 document.head.appendChild(js);
-
+//chat box
+(function() {
+  var x = document.createElement("script");
+  x.type = "text/javascript";
+  x.async = true;
+  x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.libraryh3lp.com/js/libraryh3lp.js?4949";
+  var y = document.getElementsByTagName("script")[0];
+  y.parentNode.insertBefore(x, y);
+})();
+/* Add GTM */
+var script = document.createElement('script');
+script.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NK5VV3M');";
+document.head.appendChild(script);
 /* detect current tab */
 setInterval(function() {
-      jQuery("[aria-label='BrowseSearch']" ).text("Browse Search");
-  if(document.location.href.indexOf('jsearch?') > -1) {
-    jQuery('#mainMenu a').removeClass('active');
-    jQuery("[aria-label='Journal Search']" ).addClass('active');
-  }else if(document.location.href.indexOf('CustomCourseReserves') > -1) {
-    jQuery('#mainMenu a').removeClass('active');
-    jQuery("[aria-label='Course Reserves']" ).addClass('active');
-  }else if(document.location.href.indexOf('citationlinker') > -1) {
-    jQuery('#mainMenu a').removeClass('active');
-    jQuery("[aria-label='Citation Lookup']" ).addClass('active');
-  }else if(document.location.href.indexOf('browse') > -1) {
-    jQuery('#mainMenu a').removeClass('active');
-    jQuery("[aria-label='BrowseSearch']" ).addClass('active');
-  }else if(document.location.href.indexOf('dbsearch?') > -1) {
-    jQuery('#mainMenu a').removeClass('active');
-    jQuery("[aria-label='Database Search']" ).addClass('active');
-  }
+  //fix issue with browse search text
+  jQuery("[aria-label='BrowseSearch']").text("Browse Search");
+  //array to detect current tab
+  var searchTabsList = {
+    "jsearch?": "Journal Search",
+    "CustomCourseReserves": "Course Reserves",
+    "citationlinker": "Citation Lookup",
+    "browse": "BrowseSearch",
+    "dbsearch?": "Database Search"
+  };
+  //cycle through array and set active tab
+  jQuery.each(searchTabsList, function(key, value) {
+    switch (document.location.href.indexOf(key) > -1) {
+      case true:
+        jQuery('#mainMenu a').removeClass('active');
+        jQuery("[aria-label='" + value + "']").addClass('active');
+        break;
+    }
+  });
 }, 50);

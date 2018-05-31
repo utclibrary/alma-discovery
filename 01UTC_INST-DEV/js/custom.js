cb778250a4140ded4cbcbd70a131312ac970522f
Steven D. Shelton <steven-shelton@utc.edu>
2018-05-31 08:24:25 -0400

    DEV - js to scroll to results on advanced search mobile screen

--- 01UTC_INST-DEV/js/custom.js
+++ 01UTC_INST-DEV/js/custom.js
@@ -22,6 +22,66 @@
       return vm.parentCtrl.iconLink;
     }
   }]);
+  //scroll down on mobile view advanced search
+  // change advanced search to jump to results 
+app.controller('prmAdvancedSearchAfterController', function($scope) {
+// watch to see if advanced search is there
+       var advancedSearchObs = new MutationObserver(function(mutations) {
+              mutations.forEach(function(mutation) {
+                     if (!mutation.addedNodes) return
+                     for (var i = 0; i < mutation.addedNodes.length; i++) {
+                           var node = mutation.addedNodes[i];
+
+                           if (node.nodeName == "BUTTON" && document.querySelector("prm-advanced-search .button-confirm.button-large.button-with-icon.md-button.md-primoExplore-theme.md-ink-ripple")) {
+                                  //need an id to jump to
+                                  let submitArea = document.querySelector(".advanced-search-output.layout-row.flex");
+                                  submitArea.setAttribute("id", "advancedSearchSubmitArea");
+                                  
+                                  var submitBtn = document.querySelector("prm-advanced-search .button-confirm.button-large.button-with-icon.md-button.md-primoExplore-theme.md-ink-ripple");
+                                  submitBtn.addEventListener("click", function(){
+                                         // wait for some results
+                                         var advancedSearchObs2 = new MutationObserver(function(mutations2) {
+                                                mutations2.forEach(function(mutation2) {
+                                                       if (!mutation2.addedNodes) return
+                                                       for (var i = 0; i < mutation2.addedNodes.length; i++) {
+                                                              var node = mutation2.addedNodes[i];
+                                                              if (node.nodeName == "PRM-SEARCH-RESULT-SORT-BY" && window.innerHeight < 775) {
+                                                                     window.location.hash='advancedSearchSubmitArea';
+                                                                     advancedSearchObs2.disconnect();
+                                                              }
+                                                       }
+                                                });
+                                         });
+                                                
+                                         advancedSearchObs2.observe(document.getElementsByTagName('prm-explore-main')[0], {
+                                                childList: true
+                                                , subtree: true
+                                                , attributes: false
+                                                , characterData: false
+                                         })
+                                         //end wait for some results
+                                  });
+                           }
+                     }
+              })
+       })
+       
+       advancedSearchObs.observe(document.getElementsByTagName('prm-advanced-search')[0], {
+              childList: true
+              , subtree: true
+              , attributes: false
+              , characterData: false
+       })
+});
+
+app.component('prmAdvancedSearchAfter', {
+       bindings: {
+              parentCtrl: '<'
+       },
+       controller: 'prmAdvancedSearchAfterController'
+});
+
+  // END scroll code
   //update template to include new URL for institution
   app.component('prmLogoAfter', {
     bindings: {

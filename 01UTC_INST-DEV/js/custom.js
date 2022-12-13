d626d91363846e1acc25a72341696ef9386b1251
Steven D. Shelton <steven.d.shelton@gmail.com>
2022-12-13 15:42:04 -0500

    updates chat embed to button to resolve reloading issue

--- 01UTC_INST-DEV/js/custom.js
+++ 01UTC_INST-DEV/js/custom.js
@@ -116,7 +116,7 @@ setInterval(function() {
     var x = document.createElement("script");
     x.type = "text/javascript";
     x.async = true;
-    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.libraryh3lp.com/js/libraryh3lp.js?14392";
+    x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "us.libraryh3lp.com/js/libraryh3lp.js?15293";
     var y = document.getElementsByTagName("script")[0];
     y.parentNode.insertBefore(x, y);
 }
